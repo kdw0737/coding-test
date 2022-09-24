8a5d08b13b5a3c2fc83dcf8844a310ad3fd4e9c6
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2022-09-24 17:59:28 +0900

    [Bronze I] Title: 약수, Time: 76 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]*arr[N-1]);
+        System.out.println(sb);
+    }
+}
