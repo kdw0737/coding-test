a9a6378e52b586f0fc612005b79203fa3057c253
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2022-10-03 17:33:23 +0900

    [Silver III] Title: N과 M (1), Time: 168 ms, Memory: 21132 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N,M;
+    static int[] arr;
+    static boolean[] isUsed;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        isUsed = new boolean[N];
+
+        backTracking(N,M,0);
+        System.out.println(sb);
+    }
+
+    private static void backTracking(int N, int M, int idx){
+        if(idx==M){
+            for(int num: arr){
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i =0; i<N; i++){
+            if(isUsed[i]==false){
+                isUsed[i]=true;
+                arr[idx]=i+1;
+                backTracking(N,M,idx+1);
+                isUsed[i]=false;
+            }
+        }
+    }
+}
