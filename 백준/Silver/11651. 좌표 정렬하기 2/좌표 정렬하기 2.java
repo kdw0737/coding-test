c7cd931a114e5e3ea54c45895175f25eb1fd95ff
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2022-10-10 21:13:10 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 756 ms, Memory: 74032 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,27 +1,20 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
-        StringTokenizer st ;
-        int[][] point = new int[N][2];
-        for(int i =0; i<N; i++){
-            st=new StringTokenizer(bf.readLine());
-            point[i][0]=Integer.parseInt(st.nextToken());
-            point[i][1]=Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][2];
+        for(int i = 0; i<N ; i++){
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(point, ((o1, o2) -> {
-            if(o1[1]==o2[1])
-                return Integer.compare(o1[0],o2[0]);
-            else
-                return Integer.compare(o1[1],o2[1]);
-        }));
+        Arrays.sort(arr,(o1, o2) -> o1[1]==o2[1]? o1[0]-o2[0] : o1[1] - o2[1]);
         StringBuilder sb = new StringBuilder();
         for(int i =0; i<N; i++){
-            sb.append(point[i][0]+" "+point[i][1]).append('\n');
+            sb.append(arr[i][0]+" "+arr[i][1]).append('\n');
         }
         System.out.println(sb);
     }
