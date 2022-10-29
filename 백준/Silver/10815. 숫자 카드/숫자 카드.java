55069011535384acb8d027ce99be86690c9cefd9
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2022-10-29 16:13:51 +0900

    [Silver V] Title: 숫자 카드, Time: 1032 ms, Memory: 144360 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -5,43 +5,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
-        int[] solution = new int[N];
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            solution[i] = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for(int i =0; i<N; i++){
+            arr[i]=Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(solution);
         int M = Integer.parseInt(bf.readLine());
+        st = new StringTokenizer(bf.readLine());
         int[] check = new int[M];
-        StringTokenizer st1 = new StringTokenizer(bf.readLine());
         for(int i =0; i<M; i++){
-            check[i] = Integer.parseInt(st1.nextToken());
+            check[i]=Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
-            if(BinarySearch(solution,check[i]))
-                sb.append(1).append(" ");
-            else
-                sb.append(0).append(" ");
+        for(int i =0; i<M; i++){
+            sb.append(BinarySearch(arr,check[i])).append(" ");
         }
         System.out.println(sb);
     }
-
-    public static boolean BinarySearch(int[] solution, int key) {
+    private static int BinarySearch(int[] arr,int key){
         int left = 0;
-        int right = solution.length-1;
-        int mid;
-        while (left<=right) {
-            mid = (right + left) / 2;
-            if (key == solution[mid] || key ==solution[left] || key == solution[right]) {
-                return true;
-            }
-            else if (key > solution[mid]) {
-                left = mid + 1;
-            }else {
-                right = mid - 1;
-            }
+        int right = arr.length-1;
+        int mid ;
+
+        while(left<=right){
+            mid = (left+right)/2;
+            if(arr[left] == key || arr[right] == key || key==arr[mid])
+                return 1;
+            else if(key>arr[mid])
+                left = mid+1;
+            else
+                right = mid-1;
         }
-        return false;
+        return 0;
     }
-}
+ }
