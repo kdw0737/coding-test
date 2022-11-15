ec861812a53462b7e4157d6917dcde7c6d29feb4
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2022-11-15 22:22:26 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int row, column;
-        row = column = 1;
         for(int i =1; i<=Math.sqrt(yellow); i++){
-            column = i;
-            if(yellow%i==0){
-                row = yellow/column;
-                if(yellow+brown==(row+2)*(column+2)){
-                    answer[0]=row+2;
-                    answer[1]=column+2;
-                }
+            if(yellow%i==0 && (i+2)*((yellow/i)+2)==brown+yellow){
+                answer[0]=(yellow/i)+2;
+                answer[1]=i+2;
+                break;
             }
         }
         return answer;
