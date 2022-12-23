e8ab905d772d6f40954e537bca97d2d50c127872
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2022-12-23 18:54:56 +0900

    [Silver II] Title: 좌표 압축, Time: 1808 ms, Memory: 359732 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -4,25 +4,25 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Integer,Integer> map = new HashMap<>();
         int N = Integer.parseInt(bf.readLine());
-        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(bf.readLine());
+        int[] arr = new int[N];
         for(int i =0; i<N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int[] copy_arr = Arrays.copyOf(arr,arr.length);
-        Arrays.sort(copy_arr);
-        int index =0;
-        for(int num : copy_arr){
-            if(!map.containsKey(num)){
-                map.put(num,index);
+        int[] copy = Arrays.copyOf(arr,N);
+        Arrays.sort(copy);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int index = 0;
+        for(int i =0; i<N; i++){
+            if(!map.containsKey(copy[i])) {
+                map.put(copy[i], index);
                 index++;
             }
         }
         StringBuilder sb = new StringBuilder();
-        for(int num: arr){
-            sb.append(map.get(num)).append(" ");
+        for(int n: arr){
+            sb.append(map.get(n)).append(" ");
         }
         System.out.println(sb);
     }
