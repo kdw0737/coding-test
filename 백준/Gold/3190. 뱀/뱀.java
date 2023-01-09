749a9bdd05dce9288d9338da3161ccc9d1706cc3
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2023-01-09 23:38:59 +0900

    [Gold IV] Title: 뱀, Time: 88 ms, Memory: 12032 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -2,30 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] x= {1,0,-1,0};
-    static int[] y ={0,1,0,-1};
-    static boolean[][] visited;
-    static int[][] map;
-    static Deque<int[]> deque = new ArrayDeque<>();
-    static int count = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
         int appleNum = Integer.parseInt(bf.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
+        int[] x= {1,0,-1,0};
+        int[] y ={0,1,0,-1};
+        Deque<int[]> deque = new ArrayDeque<>(); //뱀 위치정보 저장 덱 생성
+        int[][] map = new int[N][N]; // 사과 위치 정보 담을 배열
+        boolean[][] visited = new boolean[N][N]; // 현재 뱀의 몸통이 지도의 어디에 걸쳐있는지 저장
+        int count = 0;
+
+        //사과 위치 저장
         for(int i =0; i<appleNum; i++){
             StringTokenizer st = new StringTokenizer(bf.readLine());
             map[Integer.parseInt(st.nextToken())-1][Integer.parseInt(st.nextToken())-1] = 1;
         }
         int directionNum = Integer.parseInt(bf.readLine());
-        HashMap<Integer,String> directionInfo = new HashMap<>();
+        HashMap<Integer,String> directionInfo = new HashMap<>(); //방향정보 저장할 해시맵 ( key : 초, value: 방향 )
+        //해시맵에 방향정보 저장
         for(int i =0; i<directionNum ; i++){
             StringTokenizer st = new StringTokenizer(bf.readLine());
             directionInfo.put(Integer.parseInt(st.nextToken()), st.nextToken());
         }
-        int nowDir =0;
-        deque.add(new int[]{0, 0});
+        int nowDir =0; //현재 방향 인덱스로 표현
+        deque.add(new int[]{0, 0}); //출발점
         while(true){
             if(directionInfo.containsKey(count)){
                 if(directionInfo.get(count).equals("D")) {
@@ -50,6 +51,7 @@ public class Main {
                     break;
             }else
                 break;
+            //머리가 위치한곳이 사과인지 아닌지 체크, 사과인경우 0으로 만들어주고 아닌경우 몸통 길이 줄여줌
             if(map[headY][headX]!=1){
                 int[] pos = deque.poll();
                 visited[pos[0]][pos[1]] = false;
