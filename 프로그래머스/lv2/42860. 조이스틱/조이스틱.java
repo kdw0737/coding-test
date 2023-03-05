91931c08f2e95e650c765575486023ce6f688933
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2023-03-05 17:40:51 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(String name) {
+        int result = 0;
+        int move = name.length() -1;
+        for(int i =0; i<name.length(); i++){
+            result+=Math.min(name.charAt(i)-'A', 'Z'-name.charAt(i)+1);
+            int idx = i+1;
+            while(idx<name.length() && name.charAt(idx) == 'A'){
+                idx++;
+            }
+            move = Math.min(move,i*2 + name.length()-idx);
+            move = Math.min(move,(name.length()-idx)*2 + i);
+        }
+        return move+result;
+    }
+}
