09c73f7d6c9fa87b6f3b91fcfb309c2b89812fb6
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2023-03-22 21:08:00 +0900

    [Silver I] Title: 절댓값 힙, Time: 428 ms, Memory: 28728 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -6,17 +6,19 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> Math.abs(o1) == Math.abs(o2) ? o1 - o2 : Math.abs(o1) - Math.abs(o2));
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(bf.readLine());
             if (num == 0) {
                 if (!pq.isEmpty()) {
-                    System.out.println(pq.poll());
+                    sb.append(pq.poll()).append('\n');
                 } else {
-                    System.out.println(0);
+                    sb.append(0).append('\n');
                 }
             } else {
                 pq.add(num);
             }
         }
+        System.out.println(sb);
     }
 }
