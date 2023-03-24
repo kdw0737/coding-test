5e367eabd3b5d61d633dd33e95871471e28e69ba
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2023-03-24 17:08:06 +0900

    [level 2] Title: 위장, Time: 0.08 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0; i<clothes.length; i++){
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String[] arr : clothes){
+            map.put(arr[1],map.getOrDefault(arr[1],0)+1);
         }
-        int answer = 1;
+        int result =1;
         for(String key : map.keySet()){
-            answer*=map.get(key)+1;
+            result *= map.get(key)+1;
         }
-        return answer-1;
+        return result -1;
     }
 }
