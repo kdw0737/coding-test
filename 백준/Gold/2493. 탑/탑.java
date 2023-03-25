391d1b08d2611e37abc0cc5f002ff3052716ec1b
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2023-03-25 17:41:24 +0900

    [Gold V] Title: 탑, Time: 780 ms, Memory: 167476 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -4,24 +4,25 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        Stack<int[]> stack = new Stack<>();
         int N = Integer.parseInt(bf.readLine());
-        StringTokenizer st = new StringTokenizer(bf.readLine());
         StringBuilder sb = new StringBuilder();
+        Stack<int[]> stack = new Stack<>();
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
             while (!stack.isEmpty()) {
                 if (stack.peek()[0] > num) {
                     sb.append(stack.peek()[1] + " ");
                     break;
-                } else {
+                }else{
                     stack.pop();
                 }
             }
             if (stack.isEmpty()) {
-                sb.append(0+" ");
+                sb.append(0 + " ");
             }
-            stack.add(new int[]{num, i + 1});
+            stack.push(new int[]{num, i + 1});
         }
         System.out.println(sb);
     }
