5407f15761465d9788020cae6489b9f3bf296060
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2023-04-07 23:09:17 +0900

    [level 2] Title: 땅따먹기, Time: 27.83 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    int solution(int[][] land) {
+        int[][] dp = new int[land.length][4];
+        for(int i =0; i<4; i++){
+            dp[0][i] = land[0][i];
+        }
+        for(int i =1; i<land.length; i++){
+            dp[i][0] = Math.max(Math.max(dp[i-1][1],dp[i-1][2]),dp[i-1][3]) + land[i][0];
+            dp[i][1] = Math.max(Math.max(dp[i-1][0],dp[i-1][2]),dp[i-1][3]) + land[i][1];
+            dp[i][2] = Math.max(Math.max(dp[i-1][0],dp[i-1][1]),dp[i-1][3]) + land[i][2];
+            dp[i][3] = Math.max(Math.max(dp[i-1][0],dp[i-1][1]),dp[i-1][2]) + land[i][3];
+        }
+        int result = 0;
+        for(int i =0; i<4; i++){
+            if(result<dp[land.length-1][i])
+                result = dp[land.length-1][i];
+        }
+        return result;
+    }
+}
