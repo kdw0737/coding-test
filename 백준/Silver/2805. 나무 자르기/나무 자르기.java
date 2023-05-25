c8244bc31048778b71dc86c100c1958025ce8e63
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2023-05-25 20:04:27 +0900

    [Silver II] Title: 나무 자르기, Time: 496 ms, Memory: 168020 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        long len = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(bf.readLine());
+        long lower = 1;
+        long upper = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] > upper) {
+                upper = arr[i];
+            }
+        }
+        while (lower < upper) {
+            long mid = (lower + upper) / 2;
+            long sum = 0;
+            for (int n : arr) {
+                if (n > mid) {
+                    sum += (n - mid);
+                }
+            }
+            if (sum < len) {
+                upper = mid;
+            } else {
+                lower = mid + 1;
+            }
+        }
+        System.out.println(lower - 1);
+    }
+}
