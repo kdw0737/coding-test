0c149604c22125557d14b3e0ff42f921ba71ee39
김도욱 <113456098+kdw0737@users.noreply.github.com>
2024-09-26 01:49:07 +0900

    [Gold V] Title: 내려가기, Time: 304 ms, Memory: 53712 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -27,33 +27,24 @@ public class Main {
 				if (j == 0) {
 					maxArr[i + 1][0] = Math.max(maxArr[i][0] + arr[i + 1][0], maxArr[i + 1][0]);
 					maxArr[i + 1][1] = Math.max(maxArr[i][0] + arr[i + 1][1], maxArr[i + 1][1]);
+					minArr[i + 1][0] = Math.min(minArr[i][0] + arr[i + 1][0], minArr[i + 1][0]);
+					minArr[i + 1][1] = Math.min(minArr[i][0] + arr[i + 1][1], minArr[i + 1][1]);
 				}else if (j == 1) {
 					maxArr[i + 1][0] = Math.max(maxArr[i][1] + arr[i + 1][0], maxArr[i + 1][0]);
 					maxArr[i + 1][1] = Math.max(maxArr[i][1] + arr[i + 1][1], maxArr[i + 1][1]);
 					maxArr[i + 1][2] = Math.max(maxArr[i][1] + arr[i + 1][2], maxArr[i + 1][2]);
-				}else if (j == 2) {
-					maxArr[i + 1][1] = Math.max(maxArr[i][2] + arr[i + 1][1], maxArr[i + 1][1]);
-					maxArr[i + 1][2] = Math.max(maxArr[i][2] + arr[i + 1][2], maxArr[i + 1][2]);
-				}
-			}
-		}
-		max = Math.max(maxArr[N-1][0], Math.max(maxArr[N-1][1], maxArr[N-1][2]));
-
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (j == 0) {
-					minArr[i + 1][0] = Math.min(minArr[i][0] + arr[i + 1][0], minArr[i + 1][0]);
-					minArr[i + 1][1] = Math.min(minArr[i][0] + arr[i + 1][1], minArr[i + 1][1]);
-				}else if (j == 1) {
 					minArr[i + 1][0] = Math.min(minArr[i][1] + arr[i + 1][0], minArr[i + 1][0]);
 					minArr[i + 1][1] = Math.min(minArr[i][1] + arr[i + 1][1], minArr[i + 1][1]);
 					minArr[i + 1][2] = Math.min(minArr[i][1] + arr[i + 1][2], minArr[i + 1][2]);
 				}else if (j == 2) {
+					maxArr[i + 1][1] = Math.max(maxArr[i][2] + arr[i + 1][1], maxArr[i + 1][1]);
+					maxArr[i + 1][2] = Math.max(maxArr[i][2] + arr[i + 1][2], maxArr[i + 1][2]);
 					minArr[i + 1][1] = Math.min(minArr[i][2] + arr[i + 1][1], minArr[i + 1][1]);
 					minArr[i + 1][2] = Math.min(minArr[i][2] + arr[i + 1][2], minArr[i + 1][2]);
 				}
 			}
 		}
+		max = Math.max(maxArr[N-1][0], Math.max(maxArr[N-1][1], maxArr[N-1][2]));
 		min = Math.min(minArr[N-1][0], Math.min(minArr[N-1][1], minArr[N-1][2]));
 
 		System.out.println(max + " " + min);
