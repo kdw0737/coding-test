c255d6cbaeb8414a00c61dcce1a98f44b2313b45
김도욱 <113456098+kdw0737@users.noreply.github.com>
2024-10-11 21:06:31 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.74 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,45 +1,9 @@
-import java.util.*;
 class Solution {
-    static HashMap<String,String> map;
     public int solution(String s) {
-        String result = "";
-        String tmp = "";
-        map = new HashMap<>();
-        // 숫자인경우는 그대로 result에 추가 
-        // 숫자가 아닌 경우는 숫자가 나올때까지 tmp에 문자 저장
-        addNum();
-        for(int i =0; i<s.length(); i++){
-            if(Character.isDigit(s.charAt(i))){
-                //숫자인 경우
-                if(map.containsKey(tmp)){
-                    result += map.get(tmp);
-                }
-                result += String.valueOf(s.charAt(i));
-                tmp = "";
-            }else{
-                tmp += String.valueOf(s.charAt(i));
-                if(map.containsKey(tmp)){
-                    result += map.get(tmp);
-                    tmp = "";
-                }
-            }
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-        if(tmp != ""){
-            result += map.get(tmp);
-        }
-        return Integer.parseInt(result);
+        return Integer.parseInt(s);
     }
-    
-    public static void addNum(){
-        map.put("zero","0");
-        map.put("one","1");
-        map.put("two","2");
-        map.put("three","3");
-        map.put("four","4");
-        map.put("five","5");
-        map.put("six","6");
-        map.put("seven","7");
-        map.put("eight","8");
-        map.put("nine","9");
-    } 
 }
