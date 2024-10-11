c90a9982916aa5b5b19207e47319f0331da3d9b8
김도욱 <113456098+kdw0737@users.noreply.github.com>
2024-10-11 20:08:27 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.62 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,15 +1,15 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
         // 이동거리 : (큰수 - 작은수) / 3 + (큰수 - 작은수) % 3 
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int left = 10; // * 에 위치
         int right = 12; // # 에 위치
         for(int n : numbers){
             if(n == 1 || n == 4 || n == 7){
-                result += "L";
+                result.append("L");
                 left = n;
             }else if(n == 3 || n == 6 || n == 9){
-                result += "R";
+                result.append("R");
                 right = n;
             }else{ // 2 5 8 0 인 경우
                 int cur = n;
@@ -18,25 +18,25 @@ class Solution {
                 int rightDist = (Math.abs(right-cur)) / 3 + (Math.abs(right-cur)) % 3;
                 if(leftDist < rightDist){
                     // 왼손이 더 가까운경우
-                    result += "L";
+                    result.append("L");
                     left = cur;
                 }else if(leftDist > rightDist){
                     // 오른손이 더 가까운경우
-                    result += "R";
+                    result.append("R");
                     right = cur;
                 }else{
                     // 둘이 거리가 같은 경우
                     if(hand.equals("right")){
-                        result += "R";
+                        result.append("R");
                         right = cur;
                     }
                     if(hand.equals("left")){
-                        result += "L";
+                        result.append("L");
                         left = cur;
                     }
                 }
             }
         }
-        return result;
+        return result.toString();
     }
 }
