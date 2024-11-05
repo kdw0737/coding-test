2341c5fec6b2571de390cdf8816bb7311aaa2458
김도욱 <113456098+kdw0737@users.noreply.github.com>
2024-11-05 22:22:19 +0900

    [Silver II] Title: 랜선 자르기, Time: 112 ms, Memory: 15492 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,38 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int M;
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        for(int i =0; i<N; i++){
-            arr[i]=Integer.parseInt(bf.readLine());
-        }
-        Arrays.sort(arr);
-        System.out.println(BinarySearch(arr));
-    }
-    public static long BinarySearch(int[] arr){
-        long left = 1;
-        long right = arr[arr.length-1];
-        long mid;
-        long max =0;
-        while(left<=right){
-            int count =0;
-            mid = (left+right)/2;
-            for(int length : arr){
-                count+=length/mid;
-            }
-            if(count<M)
-                right = mid-1;
-            else{
-                left = mid +1;
-                max = Math.max(max,mid);
-            }
-        }
-        return max;
-    }
+	static long[] arr;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		arr = new long[K];
+		long left = 1;
+		long right = 1;
+		for (int i = 0; i < K; i++) {
+			arr[i] = Integer.parseInt(bf.readLine());
+			right = Math.max(right, arr[i]);
+		}
+		right++;
+
+		while(left < right) {
+			long mid = (left + right) / 2;
+
+			if (checkCount(mid) >= N) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		System.out.println(left - 1);
+	}
+
+	public static long checkCount(long mid) {
+		long cnt = 0;
+		for (int i = 0; i < arr.length; i++) {
+			cnt += arr[i] / mid;
+		}
+		return cnt;
+	}
 }
